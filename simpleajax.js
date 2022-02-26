4f72f2ecfa10eb6d323b10cfde2f0259dc3d47d4
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-02-26 13:39:12 +0500

    AJAX Practice Done

---
+++ simpleajax.js
@@ -0,0 +1,29 @@
+/* $(function () {
+  $("#load").click(sendAJAX);
+});
+function sendAJAX() {
+    console.log("AJAX resquest sending");
+    // below is asyncronous call
+    $.get("students.txt", handleResponse); //here 1st parameter is the relative path of the file, sencond paramete is the function which will handle the recieved file.
+    // here 1st parameter of txt file actually the API Call (API last part), which we will discuss later when we study the APIs
+    console.log("resquest sent");
+
+}
+function handleResponse(response) {
+    console.log("response received");
+    // console.log(response);
+    // $("#results").empty(); 
+    $("#results").append("\n");
+    $("#results").append(response);
+}
+ */
+// modified code (simple/may be complex but clean and smaller)
+$(function () {
+  $("#load").click(function () {
+    $.get("students.txt", function (response) {
+      // $("#results").append("<br>");
+      $("#results").empty();
+      $("#results").append(response);
+    });
+  });
+});
